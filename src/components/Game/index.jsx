fc5759a9594637073b8e4c3431ed09d88a1afd79
aonec <githib@aonec.ru>
2021-02-15 20:40:07 +0300

    commit

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Result from "../Result";
 import _ from 'lodash';
 import styled from 'styled-components'
@@ -7,6 +7,18 @@ import useSound from 'use-sound';
 import Title from "antd/es/typography/Title";
 import sound from '../../sounds/sound.mp3';
 import wrong from '../../sounds/wrong.mp3';
+
+import kitap from '../../sounds/words/kitap.mp3';
+import alma from '../../sounds/words/alma.mp3';
+import el from '../../sounds/words/el.mp3';
+import keshe from '../../sounds/words/keshe.mp3';
+import avyl from '../../sounds/words/avyl.mp3';
+import bala from '../../sounds/words/bala.mp3';
+import esh from '../../sounds/words/esh.mp3';
+import bash from '../../sounds/words/bash.mp3';
+import vakyt from '../../sounds/words/vakyt.mp3';
+import suz from '../../sounds/words/suz.mp3';
+import kon from '../../sounds/words/kon.mp3';
 
 const initialQuestions = [
     {
@@ -18,7 +30,8 @@ const initialQuestions = [
             {id: 2, text: "Книга"},
             {id: 3, text: "Яблоко"},
             {id: 4, text: "Дождь"},
-        ]
+        ],
+        audio: kitap
     },
     {
         id: 1,
@@ -29,7 +42,8 @@ const initialQuestions = [
             {id: 2, text: "Книга"},
             {id: 3, text: "Яблоко"},
             {id: 4, text: "Дождь"},
-        ]
+        ],
+        audio: alma
     },
     {
         id: 2,
@@ -40,7 +54,8 @@ const initialQuestions = [
             {id: 2, text: "Год"},
             {id: 3, text: "Яблоко"},
             {id: 4, text: "Дождь"},
-        ]
+        ],
+        audio: el
     },
     {
         id: 3,
@@ -51,7 +66,8 @@ const initialQuestions = [
             {id: 2, text: "Книга"},
             {id: 3, text: "Яблоко"},
             {id: 4, text: "Человек"},
-        ]
+        ],
+        audio: keshe
     },
     {
         id: 4,
@@ -62,7 +78,8 @@ const initialQuestions = [
             {id: 2, text: "Книга"},
             {id: 3, text: "Яблоко"},
             {id: 4, text: "Работа"},
-        ]
+        ],
+        audio: esh
     },
     {
         id: 5,
@@ -73,7 +90,8 @@ const initialQuestions = [
             {id: 2, text: "Книга"},
             {id: 3, text: "Яблоко"},
             {id: 4, text: "День"},
-        ]
+        ],
+        audio: kon
     },
     {
         id: 6,
@@ -84,7 +102,8 @@ const initialQuestions = [
             {id: 2, text: "Книга"},
             {id: 3, text: "Яблоко"},
             {id: 4, text: "Деревня"},
-        ]
+        ],
+        audio: avyl
     },
     {
         id: 7,
@@ -95,7 +114,8 @@ const initialQuestions = [
             {id: 2, text: "Книга"},
             {id: 3, text: "Яблоко"},
             {id: 4, text: "Ребенок"},
-        ]
+        ],
+        audio: bala
     },
     {
         id: 8,
@@ -106,7 +126,8 @@ const initialQuestions = [
             {id: 2, text: "Книга"},
             {id: 3, text: "Яблоко"},
             {id: 4, text: "Время"},
-        ]
+        ],
+        audio: vakyt
     },
     {
         id: 9,
@@ -117,7 +138,8 @@ const initialQuestions = [
             {id: 2, text: "Книга"},
             {id: 3, text: "Яблоко"},
             {id: 4, text: "Голова"},
-        ]
+        ],
+        audio: bash
     },
     {
         id: 10,
@@ -128,7 +150,8 @@ const initialQuestions = [
             {id: 2, text: "Книга"},
             {id: 3, text: "Яблоко"},
             {id: 4, text: "Слово"},
-        ]
+        ],
+        audio: suz
     },
 //     Ел – год, лет;
 // Кеше – человек;
@@ -149,21 +172,23 @@ const Game = () => {
             result: [],
             finished: false,
             currentQuestionIndex: 0,
-            questions: _.shuffle(initialQuestions).slice(1, 6)
+            questions: _.shuffle(initialQuestions).slice(1, 6),
         }
         const [state, setState] = useState(initialState);
-        const {currentQuestionIndex, result, questions, finished} = state
+        const {currentQuestionIndex, result, questions, finished, currentAudio} = state
+
+
         const Question = () => {
             const question = questions[currentQuestionIndex];
-            const {options, questionText, correct, id: questionId} = question
+            const {options, questionText, correct, id: questionId, audio} = question
             const shuffledOptions = _.shuffle(options)
             const [yes] = useSound(sound);
             const [no] = useSound(wrong);
+            const [word] = useSound(audio)
 
             const Options = shuffledOptions.map((option, index) => {
                 const handleClick = (id) => {
                     const timeout = window.setTimeout(() => {
-
                         const res = id === correct ? {
                             correct: true,
                             id: questionId,
@@ -188,6 +213,10 @@ const Game = () => {
 
                 }
                 const {id, text} = option;
+                // const timeout = window.setTimeout(() => {
+                //
+                //     window.clearTimeout(timeout)
+                // }, 500)
                 return <li key={id}>
                     <Button onClick={() => {
                         handleClick(id);
@@ -197,6 +226,9 @@ const Game = () => {
                 </li>
             })
 
+            useEffect(()=>{
+                word()
+            },[word])
             return (
                 <div style={{textAlign: "center"}}>
                     <div>Вопрос {currentQuestionIndex + 1} из {questions.length}</div>
@@ -207,7 +239,6 @@ const Game = () => {
                 </div>
             )
         }
-
 
 
         return (
