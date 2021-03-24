f1179b6ca8120414f43f7832b48404f74c2399f6
aonec <githib@aonec.ru>
2021-03-24 21:07:57 +0300

    Update index.jsx

--- src/components/Latin/index.jsx
+++ src/components/Latin/index.jsx
@@ -1,7 +1,70 @@
 import React, {useState} from "react";
 
 const translate = [
-    {tat: 'a', lat: 'q'}
+
+    {tat: 'A', lat: 'Ä'},
+    {tat: 'a', lat: 'ä'},
+    {tat: 'Б', lat: 'B'},
+    {tat: 'б', lat: 'b'},
+    {tat: 'В', lat: '1'},
+    {tat: 'в', lat: '1'},
+    {tat: 'Г', lat: '1'},
+    {tat: 'г', lat: '1'},
+    {tat: 'Д', lat: '1'},
+    {tat: 'д', lat: '1'},
+    {tat: 'Е', lat: '1'},
+    {tat: 'е', lat: '1'},
+    {tat: 'Ж', lat: '1'},
+    {tat: 'ж', lat: '1'},
+    {tat: 'З', lat: '1'},
+    {tat: 'з', lat: '1'},
+    {tat: 'И', lat: '1'},
+    {tat: 'и', lat: '1'},
+    {tat: 'Й', lat: '1'},
+    {tat: 'й', lat: '1'},
+    {tat: 'К', lat: '1'},
+    {tat: 'к', lat: '1'},
+    {tat: 'Л', lat: '1'},
+    {tat: 'л', lat: '1'},
+    {tat: 'М', lat: '1'},
+    {tat: 'м', lat: '1'},
+    {tat: 'Н', lat: '1'},
+    {tat: 'н', lat: '1'},
+    {tat: 'О', lat: '1'},
+    {tat: 'о', lat: '1'},
+    {tat: 'П', lat: '1'},
+    {tat: 'п', lat: '1'},
+    {tat: 'Р', lat: '1'},
+    {tat: 'р', lat: '1'},
+    {tat: 'С', lat: '1'},
+    {tat: 'с', lat: '1'},
+    {tat: 'Т', lat: '1'},
+    {tat: 'т', lat: '1'},
+    {tat: 'У', lat: '1'},
+    {tat: 'у', lat: '1'},
+    {tat: 'Ф', lat: '1'},
+    {tat: 'ф', lat: '1'},
+    {tat: 'Х', lat: '1'},
+    {tat: 'х', lat: '1'},
+    {tat: 'Ц', lat: '1'},
+    {tat: 'ц', lat: '1'},
+    {tat: '1', lat: '1'},
+    {tat: '1', lat: '1'},
+    {tat: '1', lat: '1'},
+    {tat: '1', lat: '1'},
+    {tat: '1', lat: '1'},
+    {tat: '1', lat: '1'},
+    {tat: '1', lat: '1'},
+    {tat: '1', lat: '1'},
+    {tat: '1', lat: '1'},
+    {tat: '1', lat: '1'},
+    {tat: '1', lat: '1'},
+    {tat: '1', lat: '1'},
+    {tat: '1', lat: '1'},
+    {tat: '1', lat: '1'},
+    {tat: '1', lat: '1'},
+    {tat: '1', lat: '1'},
+
     // Aa - Aa
     // Ää - Әә
     // Bb - Бб
@@ -54,9 +117,7 @@ const Latin = () => {
                 case "ә":
                     output=output + "ä"
                     break;
-                case "5":
-                    output=output + "q3"
-                    break;
+
                 default:
                     output=output + input[i]
             }
