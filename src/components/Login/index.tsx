9004c122b2a71c89051b993bf16f1dcf09edb3b9
aonec <githib@aonec.ru>
2021-04-28 01:07:19 +0300

    add some styles

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {StyledWelcome} from '../Welcome/WelcomeStyles'
+import styled from "styled-components";
 
 const Login = ({
                    user,
@@ -11,17 +12,28 @@ const Login = ({
         console.log(user.email)
         console.log(user.uid)
         return (
-            <StyledWelcome>
+            <StyledLogin>
                 <p>Здравствуйте,&nbsp;{user.displayName}</p>
+                <p>Ваша почта: {user.email}</p>
+                <p>Ваш ID: {user.uid}</p>
                 <button onClick={signOut}>Выйти</button>
-            </StyledWelcome>
+            </StyledLogin>
         )
     }
     return (
-        <StyledWelcome>
+        <StyledLogin>
             <p>Войти</p>
             <button onClick={signInWithGoogle}>Google</button>
-        </StyledWelcome>
+        </StyledLogin>
     )
 }
 export default Login
+
+const StyledLogin = styled.div`
+  color: #718CCC;
+  display: flex;
+  width: min-content;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+`
