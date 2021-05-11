4226181f9773c045b7898a0a6aad98f710c4463f
maratismodest <maratismodest@gmail.com>
2021-05-11 12:16:07 +0300

    add test burger menu will use sometime

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -3,26 +3,23 @@ import { isMobile } from "react-device-detect";
 import MobileMenu from "./MobileMenu";
 import { DesktopMenu } from "./DesktopMenu";
 import { slide as Menu } from "react-burger-menu";
-
+import "./styles.scss";
 export default ({ user }: any) => {
-  const showSettings = () => {
-    console.log("test");
-  };
-  return (
-    <Menu>
-      <a id="home" className="menu-item" href="/">
-        Home
-      </a>
-      <a id="about" className="menu-item" href="/about">
-        About
-      </a>
-      <a id="contact" className="menu-item" href="/contact">
-        Contact
-      </a>
-      <a onClick={showSettings} className="menu-item--small" href="">
-        Settings
-      </a>
-    </Menu>
-  );
+  console.log(isMobile);
+  return isMobile ? <MobileMenu user={user} /> : <DesktopMenu />;
+  //   <Menu right>
+  //     <a id="home" className="menu-item" href="/">
+  //       Home
+  //     </a>
+  //     <a id="about" className="menu-item" href="/about">
+  //       About
+  //     </a>
+  //     <a id="contact" className="menu-item" href="/contact">
+  //       Contact
+  //     </a>
+  //     <a onClick={showSettings} className="menu-item--small" href="">
+  //       Settings
+  //     </a>
+  //   </Menu>
+  // );
 };
-// isMobile ? <MobileMenu user={user} /> : <DesktopMenu />;
