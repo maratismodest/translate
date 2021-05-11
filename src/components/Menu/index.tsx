626f74bd0665926a1940b9bc0c626730e02f79b8
maratismodest <maratismodest@gmail.com>
2021-05-11 11:33:57 +0300

    mobile desktop plugin and burger menu added

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -2,6 +2,27 @@ import React from "react";
 import { isMobile } from "react-device-detect";
 import MobileMenu from "./MobileMenu";
 import { DesktopMenu } from "./DesktopMenu";
+import { slide as Menu } from "react-burger-menu";
 
-export default ({ user }: any) =>
-  isMobile ? <MobileMenu user={user} /> : <DesktopMenu />;
+export default ({ user }: any) => {
+  const showSettings = () => {
+    console.log("test");
+  };
+  return (
+    <Menu>
+      <a id="home" className="menu-item" href="/">
+        Home
+      </a>
+      <a id="about" className="menu-item" href="/about">
+        About
+      </a>
+      <a id="contact" className="menu-item" href="/contact">
+        Contact
+      </a>
+      <a onClick={showSettings} className="menu-item--small" href="">
+        Settings
+      </a>
+    </Menu>
+  );
+};
+// isMobile ? <MobileMenu user={user} /> : <DesktopMenu />;
