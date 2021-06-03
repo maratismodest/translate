3c40d4ce1a3de9fd7c75ff96bdad3d6193618945
aonec <githib@aonec.ru>
2021-06-03 23:49:33 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,57 @@
-import "antd/dist/antd.css";
-import "./App.css";
-import { NavLink, Route, Switch } from "react-router-dom";
-import React, { useState } from "react";
-import i18n from "i18next";
-import withFirebaseAuth from "react-with-firebase-auth";
-import { YMInitializer } from "react-yandex-metrika";
-import firebase from "firebase/app";
-import { Spin } from "antd";
-import { initialState } from "./localBase/base";
-import { translateBaseI18 } from "./localBase/translate";
-import Welcome from "./components/Welcome";
-import Words from "./components/Words";
-import Result from "./components/Result";
-import Phrases from "./components/Phrases";
-import Collect from "./components/Collect";
-import Menu from "./components/Menu";
-import { StyledLogo } from "./AppStyles";
-import classes from "./App.module.scss";
-import Latin from "./components/Latin";
-import "firebase/analytics";
-import "firebase/auth";
-import "firebase/firestore";
-import "firebase/storage";
-import AppContext from "./AppContext";
-import User from "./components/User";
-import Word from "./components/Word";
-import { app } from "./base";
-import PickGame from "./components/PickGame";
-import ModalLogin from "./components/Modals/ModalLogin";
-import { Course } from "./components/Course";
+import React, { useState } from 'react'
+import 'antd/dist/antd.css'
+import './App.css'
+import { NavLink, Route, Switch } from 'react-router-dom'
 
-function App(props: any) {
-  const [state, setState] = useState(initialState);
-  const [modalLoginVisible, setModalVisible] = useState(false);
+import i18n from 'i18next'
+import withFirebaseAuth from 'react-with-firebase-auth'
+import { YMInitializer } from 'react-yandex-metrika'
+import firebase from 'firebase/app'
+import { Spin } from 'antd'
+import { initialState } from './localBase/base'
+import { translateBaseI18 } from './localBase/translate'
+import Welcome from './components/Welcome'
+import Words from './components/Words'
+import Result from './components/Result'
+import Phrases from './components/Phrases'
+import Collect from './components/Collect'
+import Menu from './components/Menu'
+import { StyledLogo } from './AppStyles'
+import classes from './App.module.scss'
+import Latin from './components/Latin'
+import 'firebase/analytics'
+import 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/storage'
+import AppContext from './AppContext'
+import User from './components/User'
+import Word from './components/Word'
+import { app } from './base'
+import PickGame from './components/PickGame'
+import ModalLogin from './components/Modals/ModalLogin'
+import { Course } from './components/Course'
+
+function App (props: any) {
+  const [state, setState] = useState(initialState)
+  const [modalLoginVisible, setModalVisible] = useState(false)
   i18n
     .init({
       resources: translateBaseI18,
-      lng: state.language,
+      lng: state.language
     })
-    .then();
+    .then()
 
   const context = {
     state,
     setState,
     app,
     modalLoginVisible,
-    setModalVisible,
-  };
-  if (!app) {
-    return <Spin />;
+    setModalVisible
   }
-  const { user } = props;
+  if (!app) {
+    return <Spin />
+  }
+  const { user } = props
 
   return (
     <AppContext.Provider value={context}>
@@ -66,25 +67,25 @@ function App(props: any) {
 
         <div className={classes.main} id="page-wrap">
           <Switch>
-            <Route path={["/word", "/*/word"]} render={() => <Word />} />
-            <Route path={["/words", "/*/words"]} render={() => <Words />} />
+            <Route path={['/word', '/*/word']} render={() => <Word />} />
+            <Route path={['/words', '/*/words']} render={() => <Words />} />
             <Route
-              path={["/phrases", "/*/phrases"]}
+              path={['/phrases', '/*/phrases']}
               render={() => <Phrases />}
             />
             <Route
-              path={["/collect", "/*/collect"]}
+              path={['/collect', '/*/collect']}
               render={() => <Collect />}
             />
             <Route
-              path={["/result", "/*/result"]}
+              path={['/result', '/*/result']}
               render={() => <Result {...props} />}
             />
             <Route
-              path={["/about", "/*/about"]}
+              path={['/about', '/*/about']}
               render={() => <h1>About</h1>}
             />
-            <Route path={["/latin", "/*/latin"]} render={() => <Latin />} />
+            <Route path={['/latin', '/*/latin']} render={() => <Latin />} />
             <Route path="/user" render={() => <User {...props} />} />
             <Route path="/pickgame" exact render={() => <PickGame />} />
             <Route path="/" exact render={() => <Welcome />} />
@@ -100,14 +101,14 @@ function App(props: any) {
         />
       </div>
     </AppContext.Provider>
-  );
+  )
 }
 
-const firebaseAppAuth = app.auth();
+const firebaseAppAuth = app.auth()
 const providers = {
   googleProvider: new firebase.auth.GoogleAuthProvider(),
   signInWithEmailAndPassword: new firebase.auth.EmailAuthProvider(),
-  facebookProvider: new firebase.auth.FacebookAuthProvider(),
-};
+  facebookProvider: new firebase.auth.FacebookAuthProvider()
+}
 
-export default withFirebaseAuth({ firebaseAppAuth, providers })(App);
+export default withFirebaseAuth({ firebaseAppAuth, providers })(App)
