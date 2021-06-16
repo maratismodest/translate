5d079f17e064f60643c48f02b954f792da61d9c2
aonec <githib@aonec.ru>
2021-06-16 04:59:49 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -8,8 +8,8 @@ import { AuthProvider } from './provider/AuthProvider'
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
-  // uri: 'https://server-chamala.herokuapp.com/graphql',
-  uri: 'http://localhost:4000/graphql',
+  uri: 'https://server-chamala.herokuapp.com/graphql',
+  // uri: 'http://localhost:4000/graphql',
   cache: new InMemoryCache()
 })
 ReactDOM.render(
